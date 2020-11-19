776e2283d38ab9105639d951d87ca1f6cc16b465
Mabuza98 <70321095+Mabuza98@users.noreply.github.com>
2020-11-19 15:15:35 +0200

    Add project files.

---
+++ MyWhatsAppStatusSaver/StatusSaverMasterDetailPageMaster.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MyWhatsAppStatusSaver
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class StatusSaverMasterDetailPageMaster : ContentPage
+    {
+        public ListView ListView;
+
+        public StatusSaverMasterDetailPageMaster()
+        {
+            InitializeComponent();
+
+            BindingContext = new StatusSaverMasterDetailPageMasterViewModel();
+            ListView = MenuItemsListView;
+        }
+
+        class StatusSaverMasterDetailPageMasterViewModel : INotifyPropertyChanged
+        {
+            public ObservableCollection<StatusSaverMasterDetailPageMasterMenuItem> MenuItems { get; set; }
+
+            public StatusSaverMasterDetailPageMasterViewModel()
+            {
+                MenuItems = new ObservableCollection<StatusSaverMasterDetailPageMasterMenuItem>(new[]
+                {
+                    new StatusSaverMasterDetailPageMasterMenuItem { Id = 0, Title = "Remove Ads" },
+                    new StatusSaverMasterDetailPageMasterMenuItem { Id = 1, Title = "Send feedback" },
+                    new StatusSaverMasterDetailPageMasterMenuItem { Id = 2, Title = "Rate us" },
+                    new StatusSaverMasterDetailPageMasterMenuItem { Id = 3, Title = "Share app" },
+                    new StatusSaverMasterDetailPageMasterMenuItem { Id = 4, Title = "About us" },
+                });
+            }
+
+            #region INotifyPropertyChanged Implementation
+            public event PropertyChangedEventHandler PropertyChanged;
+            void OnPropertyChanged([CallerMemberName] string propertyName = "")
+            {
+                if (PropertyChanged == null)
+                    return;
+
+                PropertyChanged.Invoke(this, new PropertyChangedEventArgs(propertyName));
+            }
+            #endregion
+        }
+    }
+}
---
+++ MyWhatsAppStatusSaver/StatusSaverMasterDetailPageMasterMenuItem.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyWhatsAppStatusSaver
+{
+    public class StatusSaverMasterDetailPageMasterMenuItem
+    {
+        public StatusSaverMasterDetailPageMasterMenuItem()
+        {
+            TargetType = typeof(StatusSaverMasterDetailPageMasterMenuItem);
+        }
+        public int Id { get; set; }
+        public string Title { get; set; }
+
+        public Type TargetType { get; set; }
+    }
+}
